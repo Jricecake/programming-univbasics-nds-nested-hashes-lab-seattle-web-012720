0c51ca445ed1312295ee6b66fd17f08b0b61c118
Jonathan Riecke <jonathanriecke@gmail.com>
2020-01-14 21:54:41 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,6 +13,8 @@ def assembled_aoh
 end
 
 def literal_aoh
+    array = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+    return array
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
